cb48c5401a1cedec02d3f269934d113f9f58dade
ศักรินทร์ สิงห์อยู่ <sekkri1234@gmail.com>
2023-10-18 16:48:35 +0700

    feat: remove upload file

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,8 +3,6 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from 'src/users/users.module';
-import { MulterModule } from '@nestjs/platform-express';
-import { storage } from '../utils/storage';
 
 @Module({
   controllers: [AuthController],
@@ -13,20 +11,7 @@ import { storage } from '../utils/storage';
     UsersModule,
     JwtModule.register({
       global: true,
-    }),
-    MulterModule.register({
-      storage: storage('/profile'),
-      fileFilter(req, file, callback) {
-        const typeArray = file.mimetype.split('/');
-        const fileType = typeArray[1];
-        if (fileType == 'jpg' || fileType == 'png' || fileType == 'pdf') {
-          callback(null, true);
-        } else {
-          callback(null, false);
-        }
-      },
-      // limits
-    }),
+    })
   ],
 })
 export class AuthModule {}
