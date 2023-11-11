145685052f3405e69e43bfb175be47cb0393ba85
ศักรินทร์ สิงห์อยู่ <sekkri1234@gmail.com>
2023-11-11 15:48:21 +0700

    feat: allow cors url

--- src/utils/allowedOrigins.ts
+++ src/utils/allowedOrigins.ts
@@ -3,5 +3,5 @@ export const allowedOrigins = [
   'http://127.0.0.1:5173',
   'http://localhost:3000',
   'https://slate-gray-chiton-slip.cyclic.app',
-  'https://frontend-books-eight.vercel.app/',
+  'https://frontend-books-2gosgzrm3-aumnatthacha.vercel.app',
 ];
